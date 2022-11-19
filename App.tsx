86bc293c3972364336cd774405ac3eb9cc7eb905
Samuel Camps <samuel.camps@student.ap.be>
2022-11-20 00:18:55 +0100

    interface aangepast

--- App.tsx
+++ App.tsx
@@ -2,29 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { GlobalQuote, StockData } from './types';
+import { StockData } from './types';
 
 export default function App() {
-  const [stockData, setStockData] = useState<GlobalQuote>();
+  const [stockData, setStockData] = useState<StockData>();
 
   useEffect(() => {
     getStockData();
   }, []);
 
   const getStockData = async () => {
-    let response = await fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey=A7ESV77V11YJI2U0');
+    let response = await fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GOOGL&apikey=A7ESV77V11YJI2U0');
     let result = await response.json();
 
-    setStockData(result['Global Quote'] as GlobalQuote);
+    setStockData(result);
   }
 
   return (
     <View style={styles.container}>
+
       <Text style={{fontSize:30}}>Stock Shock !!!</Text>
       
-      <Text>{`Symbool: ${stockData?.['01. symbol']}`}</Text>
-      <Text>{`Prijs: ${stockData?.['05. price']}`}</Text>
-      <Text>{`Datum: ${stockData?.['07. latest trading day']}`}</Text>    
+      <Text>{`Symbool: ${stockData?.['Global Quote']['01. symbol']}`}</Text>
+      <Text>{`Prijs: ${stockData?.['Global Quote']['05. price']}`}</Text>
+      <Text>{`Datum: ${stockData?.['Global Quote']['07. latest trading day']}`}</Text>    
       <StatusBar style="auto" />
     </View>
   );
--- types.ts
+++ types.ts
@@ -1,18 +1,16 @@
 export interface StockData {
-    
-    "Global Quote": GlobalQuote,
-   
-}
-export interface GlobalQuote{
 
-"01. symbol": string,
-"02. open": string,
-"03. high": string,
-"04. low": string,
-"05. price": string,
-"06. volume": string,
-"07. latest trading day": string,
-"08. previous close": string
-"09. change": string,
-"10. change percent": string
+    "Global Quote": {
+        "01. symbol": string,
+        "02. open": string,
+        "03. high": string,
+        "04. low": string,
+        "05. price": string,
+        "06. volume": string,
+        "07. latest trading day": string,
+        "08. previous close": string
+        "09. change": string,
+        "10. change percent": string
+    }
 }
+
