7d97c3ca2d157aaa110c29e7a387e6f49dfff438
Samuel Camps <samuel.camps@student.ap.be>
2022-11-20 00:37:20 +0100

    kleine aanpassingen

--- App.tsx
+++ App.tsx
@@ -4,28 +4,30 @@ import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StockData } from './types';
 
-export default function App() {
+const App = () => {
   const [stockData, setStockData] = useState<StockData>();
-
-  useEffect(() => {
-    getStockData();
-  }, []);
 
   const getStockData = async () => {
     let response = await fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GOOGL&apikey=A7ESV77V11YJI2U0');
     let result = await response.json();
 
-    setStockData(result);
+    setStockData(result as StockData);
   }
+
+  useEffect(() => {
+    getStockData();
+  }, []);
+
 
   return (
     <View style={styles.container}>
 
-      <Text style={{fontSize:30}}>Stock Shock !!!</Text>
-      
+      <Text style={{ fontSize: 30 }}>Stock Shock !!!</Text>
+
       <Text>{`Symbool: ${stockData?.['Global Quote']['01. symbol']}`}</Text>
       <Text>{`Prijs: ${stockData?.['Global Quote']['05. price']}`}</Text>
-      <Text>{`Datum: ${stockData?.['Global Quote']['07. latest trading day']}`}</Text>    
+      <Text>{`Datum: ${stockData?.['Global Quote']['07. latest trading day']}`}</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -39,3 +41,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
