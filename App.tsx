5d94fed0fa7a97671cbecf41d2875a8431c489e2
ValerieSleurs <sleurs.valerie@gmail.com>
2022-11-23 21:18:32 +0100

    Icons toegevoegd

--- App.tsx
+++ App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome } from "@expo/vector-icons";
+import { Foundation } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Search from './Search';
 
 const HomeScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home</Text>
+      <Text>Home / Favorites</Text>
     </View>
   );
 }
@@ -15,6 +19,7 @@ const SearchScreen = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Search</Text>
+      <Search />
     </View>
   );
 }
@@ -33,9 +38,18 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Search" component={SearchScreen} />        
-        <Tab.Screen name="Portfolio" component={PortfolioScreen} />
+        <Tab.Screen name="Favorites" component={HomeScreen} options={{
+          tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name="bank-transfer" size={33} color="black" />
+        }}
+        />
+        <Tab.Screen name="Search" component={SearchScreen} options={{
+          tabBarIcon: ({ color, size }) => <FontAwesome name="search" size={23} color="black" />
+        }}
+        />
+        <Tab.Screen name="Portfolio" component={PortfolioScreen} options={{
+          tabBarIcon: ({ color, size }) => <Foundation name="dollar-bill" size={32} color="black" />
+        }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
