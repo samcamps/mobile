ae3b2531bc8a1f75a339695110ea26b573b83778
ValerieSleurs <sleurs.valerie@gmail.com>
2022-11-29 14:28:14 +0100

    recentste stockdata favorites adhv API call

--- components/Favorites/FavoritesTile.tsx
+++ components/Favorites/FavoritesTile.tsx
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { FavoritesTileProps } from '../../types';
+import { FavoritesTileProps, StockData } from '../../types';
 
 const FavoritesTile = ({ symbol }: FavoritesTileProps) => {
 
-    //GetQuote(): ophalen data per favorite
+    const [stockData, setStockData] = useState<StockData>();
+
+    const getStockData = async () => {
+        let response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=A7ESV77V11YJI2U0`);
+        let result = await response.json();
+
+        setStockData(result as StockData);
+    }
+
+    useEffect(() => {
+        getStockData();
+    }, []);
+
+    console.log(stockData);
 
     return (
 
         <View style={styles.container}>
             <Pressable style={styles.pressable} >
 
-                <Text>{`Symbol: ${symbol}`}</Text>
+                <Text>{`Symbool: ${stockData?.['Global Quote']['01. symbol']}`}</Text>
+                <Text>{`Prijs: ${stockData?.['Global Quote']['05. price']}`}</Text>
+                <Text>{`Datum: ${stockData?.['Global Quote']['07. latest trading day']}`}</Text>
 
             </Pressable>
         </View>
